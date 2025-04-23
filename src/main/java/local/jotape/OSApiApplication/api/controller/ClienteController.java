17da9f4caebfd1d309377836aa6171bdd744f4cd
jpmacielprado <jp.macielprado@gmail.com>
2025-04-23 10:44:53 -0300

    O.S. API pt3 -Criado o ClienteRepository; -Atualização na classe ClienteController (adicianado busca por ID, POST, PUT e DELETE); -Atualização na classe Cliente (incrementado o @NotBlank);
    No PotMan tudo OK.

--- src/main/java/local/jotape/OSApiApplication/api/controller/ClienteController.java
+++ src/main/java/local/jotape/OSApiApplication/api/controller/ClienteController.java
@@ -8,24 +8,85 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import local.jotape.OSApiApplication.domain.model.Cliente;
+import local.jotape.OSApiApplication.domain.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class ClienteController {
     
+    @Autowired
+    private ClienteRepository clienteRepository;
+    
     @PersistenceContext
     private EntityManager manager;
-    
     
     @GetMapping("/clientes")
     public List<Cliente> listas() {
         
-        //Linguagem JPQL (tipo SQL só que do Jakarta
+        //Linguagem JPQL (tipo SQL só que em Jakarta)
         return manager.createQuery("from Cliente", Cliente.class)
                 .getResultList();
     }
     
-}
+    
+    
+    @GetMapping("/clientes/{clienteID}")
+    public ResponseEntity<Cliente> buscar(@PathVariable Long clienteID) {
+        
+        Optional<Cliente> cliente = clienteRepository.findById(clienteID);
+        
+        if (cliente.isPresent()){
+            return ResponseEntity.ok(cliente.get());
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+    @PostMapping("/clientes")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente adicionar(@RequestBody Cliente cliente) {
+        
+        return clienteRepository.save(cliente);
+        
+    }
+    
+    @PutMapping("/clientes/{clienteID}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteID, @RequestBody Cliente cliente) {
+        
+        //Verifica se o cliente existe
+        if (!clienteRepository.existsById(clienteID)) {
+            return ResponseEntity.notFound().build();
+        }
+        
+        cliente.setId(clienteID);
+        cliente = clienteRepository.save(cliente);
+        return ResponseEntity.ok(cliente);
+                
+    }
+    
+    @DeleteMapping("/cliente/{clienteID}")
+    public ResponseEntity<Void> excluir(@PathVariable Long clienteID) {
+        //Verifica se o cliente existe ou não
+        
+        if (!clienteRepository.existsById(clienteID)){
+            return ResponseEntity.notFound().build();
+        }
+        
+        clienteRepository.deleteById(clienteID);
+        return ResponseEntity.noContent().build();
+        }
+    }
+
---
+++ src/main/java/local/jotape/OSApiApplication/domain/repository/ClienteRepository.java
@@ -0,0 +1,18 @@
+package local.jotape.OSApiApplication.domain.repository;
+
+import java.util.List;
+import local.jotape.OSApiApplication.domain.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author sesideva
+ */
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    
+    List<Cliente> findByNome(String nome);
+    List<Cliente> findByNomeContaining(String nome);
+    
+}
